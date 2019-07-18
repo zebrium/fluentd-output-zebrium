b59c83eabd887795d6a7f98eb98189927ab2fbee
Brady Zuo <brady@zebrium.com>
2019-07-18 10:40:39 -0700

    Initial checkin again

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ PLUGIN_NAME="fluent-plugin-zebrium_output"
 
 echo "Building for tag $VERSION, modify .gemspec file..."
 
-echo "Install bundler..."
-bundle install
-
 echo "Build gem $PLUGIN_NAME $VERSION..."
 gem build $PLUGIN_NAME
 
