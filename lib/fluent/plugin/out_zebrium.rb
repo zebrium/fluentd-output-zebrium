79fb340c62a791e6bc0007dacdecd99a61a45e27
Brady Zuo <brady@zebrium.com>
2020-04-24 17:30:02 -0700

    Up log collector version

--- lib/fluent/plugin/out_zebrium.rb
+++ lib/fluent/plugin/out_zebrium.rb
@@ -6,7 +6,7 @@ require 'uri'
 require 'json'
 require 'docker'
 
-$ZLOG_COLLECTOR_VERSION = '1.27.0'
+$ZLOG_COLLECTOR_VERSION = '1.28.0'
 
 class Fluent::Plugin::Zebrium < Fluent::Plugin::Output
   Fluent::Plugin.register_output('zebrium', self)
